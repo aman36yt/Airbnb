652cc3c3b2261864d9f1595848ba1e34be64c688
Aman <amanbhai36yt@gmail.com>
2026-08-27 13:11:41 +0530

    models

---
+++ models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:{
+        type:String,
+        require:true
+    },
+    description:{
+        type:String,
+    },
+    img:{
+        type:String,
+        set:(v)=> v === ""? "https://plus.unsplash.com/premium_photo-1715293871661-ae60716351cc?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    },
+    price:{
+        type:String,
+    },
+    location:{
+        type:String,
+    },
+    country:{
+        type:String
+    }
+});
