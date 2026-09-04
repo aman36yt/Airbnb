e776bf1dd4767137a53573c16916a63eec22d3f4
Aman <amanbhai36yt@gmail.com>
2026-09-05 00:14:14 +0530

    feature

--- models/listing.js
+++ models/listing.js
@@ -11,9 +11,8 @@ const listingSchema = new Schema({
         type:String,
     },
     img:{
-        type:String,
-        default:"https://plus.unsplash.com/premium_photo-1715293871661-ae60716351cc?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set:(v)=> v === ""? "https://plus.unsplash.com/premium_photo-1715293871661-ae60716351cc?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        url:String,
+        filename:String,
     },
     price:{
         type:Number,
--- routes/review.js
+++ routes/review.js
@@ -8,29 +8,16 @@ const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
 const {validateReview, isLoggedin, isReviewAuthor} = require("../middleware.js");
 
+const reviewsController = require("../controllers/reviews.js");
 
 //reviews route
-router.post("/",isLoggedin,validateReview,wrapAsync(async(req,res)=>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    listing.reviews.push(newReview);
-    newReview.author = req.user._id;
-    await newReview.save();
-    await listing.save();
-    // console.log(newReview);
-    req.flash("success","New Reveiew Created!");
-    res.redirect(`/listings/${listing._id}`);
-}));
+router.post("/",isLoggedin,validateReview,wrapAsync(reviewsController.createReviews));
 
 //delete review route
 router.delete("/:reviewId",isLoggedin,
-    isReviewAuthor,wrapAsync(async(req,res)=>{
-    let {id,reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("delete","One Review deleted!");
-    res.redirect(`/listings/${id}`);
-}));
+    isReviewAuthor,
+    wrapAsync(reviewsController.destroyReview)
+);
 
 
 
